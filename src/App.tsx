eb7be76e21c06913b85ac7eb785aafc474ccc5cc
Aleksandra Baibik <abaibik@gmail.com>
2023-03-18 21:03:14 +0100

    fix cra + style header

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./App.css";
+import logo from "./images/logo.svg";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main-header">
+        <img src={logo} alt="Logo Dynasty"></img>
+        <div className="navbar-menu">
+          <FontAwesomeIcon icon={faBars} />
+        </div>
       </header>
     </div>
   );
